c158d56f865b4804a067dcf60d8dcd9b5145d2a9
Nikita Vasiliev <mr.nikitausersan@mail.ru>
2022-11-12 01:33:16 +0300

    work with context and working with validation

--- avia/src/App.js
+++ avia/src/App.js
@@ -1,12 +1,35 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Search } from './components/search/search';
+import { Info } from './components/info/info';
+import React from 'react';
+
+export const AppContext = React.createContext({});
+
 function App() {
+  const [cityFrom, setCityFrom] = React.useState('');
+  const [cityTo, setCityTo] = React.useState('');
+  const [dataFrom, setDataFrom] = React.useState('');
+  const [dataTo, setDataTo] = React.useState('');
+
   return (
-    <Routes>
-      <Route path="/" element={<Search />} />
-      {/* <Route path="/avia/info" exact element={<Info />} /> */}
-    </Routes>
+    <AppContext.Provider
+      value={{
+        cityFrom,
+        setCityFrom,
+        cityTo,
+        setCityTo,
+        dataFrom,
+        setDataFrom,
+        dataTo,
+        setDataTo,
+      }}>
+      <Routes>
+        <Route exct path="/" element={<Navigate to="/avia" />} />
+        <Route path="/avia" element={<Search />} />
+        <Route path="/avia/info" exact element={<Info />} />
+      </Routes>
+    </AppContext.Provider>
   );
 }
 
--- avia/src/components/info/info.js
+++ avia/src/components/info/info.js
@@ -1,5 +1,68 @@
 import React from 'react';
+import './info.scss';
+import { AppContext } from '../../App';
 
-export function Info(props) {
-  return <div>Info</div>;
+export function Info() {
+  const { cityFrom, cityTo, dataTo } = React.useContext(AppContext);
+
+  const times = ['09:20-11:05', '10:20-12:05', '11:20-13:00'];
+  const [activeTime, setActiveTime] = React.useState(0);
+
+  const onClickTime = (id) => {
+    setActiveTime(id);
+  };
+
+  return (
+    <div className="avia-list-item">
+      <div className="avia-list-company">
+        <div className="avia-type">Невозвратный</div>
+        <img src={process.env.PUBLIC_URL + '/img/logo.png'} alt="logo" height={39} width={39} />
+        <p>S7 Airlines</p>
+      </div>
+      <div className="avia-main-part">
+        <div className="avia-list-duration">
+          <div className="avia-options-from">
+            <p className="time-from">
+              {times[activeTime].slice(0, times[activeTime].indexOf('-'))}
+            </p>
+            <p className="city-from">{cityFrom}</p>
+            <p className="date-from">{dataTo}</p>
+          </div>
+          <div className="duration-scheme">
+            <img src={process.env.PUBLIC_URL + '/img/trip.svg'} alt="trip" />
+            <p className="duration">В пути 1ч 55 мин</p>
+          </div>
+          <div className="avia-options-to">
+            <p className="time-to">
+              {times[activeTime].slice(
+                times[activeTime].indexOf('-') + 1,
+                times[activeTime].length,
+              )}
+            </p>
+            <p className="city-to">{cityTo}</p>
+            <p className="date-to">{dataTo}</p>
+          </div>
+          <img
+            src={process.env.PUBLIC_URL + '/img/bagage.png'}
+            height={110}
+            width={50}
+            alt="trip"
+          />
+        </div>
+        <ul className="avia-time-options">
+          {times.map((time, index) => {
+            return (
+              <li
+                onClick={() => onClickTime(index)}
+                key={index}
+                className={activeTime === index ? `avia-time-button active` : `avia-time-button`}>
+                {time}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="avia-price">4 150 ₽</div>
+    </div>
+  );
 }
